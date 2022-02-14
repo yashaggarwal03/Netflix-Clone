505b9c1aeff33cf37df38092696e5d96e137e589
Yash <yash.agg2000@gmail.com>
2022-02-15 00:19:26 +0530

    Added Row Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Row from "./Row";
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hey! Let's Build Netflix Clone Front-end Today</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
