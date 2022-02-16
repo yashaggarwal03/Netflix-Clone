a0513239a6876b92e2c66408cb7454db4c104553
Yash <yash.agg2000@gmail.com>
2022-02-17 00:39:33 +0530

    Banner Component

--- src/Banner.js
+++ src/Banner.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -21,6 +22,10 @@ function Banner() {
 
   console.log(movie);
 
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+
   return (
     <header
       className="banner"
@@ -31,10 +36,20 @@ function Banner() {
       }}
     >
       <div className="banner_contents">
-        {/* title */}
-        {/* div > 2 buttons */}
-        {/* description */}
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_title}
+        </h1>
+
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+
+        <h1 className="banner_description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
+      <div className="banner_fadeBottom"/>
     </header>
   );
 }
