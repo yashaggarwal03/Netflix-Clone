41db6bdf0d7f4d130055b3ac9181f941b866be4d
Yash <yash.agg2000@gmail.com>
2022-02-18 23:29:48 +0530

    Navbar Component

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav_avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+};
+
+export default Navbar;
