7fc6a0ec3e6a2dbdd210f68509327709f0e6c888
Yash <yash.agg2000@gmail.com>
2022-02-20 10:43:39 +0530

    Added API Requests

--- src/requests.js
+++ src/requests.js
@@ -5,11 +5,16 @@ const requests = {
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}with_genres=35`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-  fetchAnimation: `/discover/movie/?api_key=${API_KEY}&with_genres=16&append_to_response=videos`
+  fetchAnimation: `/discover/movie/?api_key=${API_KEY}&with_genres=16`,
+  fetchAdventure: `/discover/movie?api_key=${API_KEY}&with_genres=12`,
+  fetchCrimeMovies: `/discover/movie?api_key=${API_KEY}&with_genres=80`,
+  fetchScienceFiction: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  fetchThrillerMovies: `/discover/movie?api_key=${API_KEY}&with_genres=53`,
+  fetchDramas: `/discover/movie?api_key=${API_KEY}&with_genres=18`,
 };
 
 export default requests;
