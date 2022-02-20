3d004e59549b5b42aadf208cc896df047d86d63f
Yash <yash.agg2000@gmail.com>
2022-02-20 10:43:03 +0530

    Final Touches

--- src/App.js
+++ src/App.js
@@ -19,11 +19,16 @@ function App() {
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      {/* <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/> */}
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Sci-Fi" fetchUrl={requests.fetchScienceFiction} />
+      <Row title="Thriller Movies" fetchUrl={requests.fetchThrillerMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
       <Row title="Animations" fetchUrl={requests.fetchAnimation} />
+      <Row title="Adventure" fetchUrl={requests.fetchAdventure} />
+      <Row title="Crime Movies" fetchUrl={requests.fetchCrimeMovies} />
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Dramas" fetchUrl={requests.fetchDramas} />
     </div>
   );
 }
